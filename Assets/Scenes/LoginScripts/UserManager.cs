2d899864996af7e00bbf3d93945faf30144bfacd
Jordy Granados <cjgranadosc@gmail.com>
2025-04-11 08:49:41 -0600

    dasd

--- Assets/Scenes/LoginScripts/UserManager.cs
+++ Assets/Scenes/LoginScripts/UserManager.cs
@@ -29,4 +29,15 @@ public class UserManager : MonoBehaviour
     public void ClearUser() {
         CurrentUserId = null;
     }
+
+    public void SetCurrentUser2(int id)
+    {
+        CurrentUserId = id;
+    }
+
+    public int GetCurrentUser2()
+    {
+        return CurrentUserId ?? -1;
+    }
+
 } 

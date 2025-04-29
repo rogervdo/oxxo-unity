ab90258282605602cf054adbb7e6b425f6bd9c4b
Pablo Zapata <tamtok2.0@gmail.com>
2025-04-29 00:52:19 -0600

    Logros Funcionales (MVP del team)

---
+++ Assets/Menu/MenuScripts/ClasesAPi/LogroAsset.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[System.Serializable]
+public class LogroAsset
+{
+    public int id_logro;
+    public GameObject asset;
+}
---
+++ Assets/Menu/MenuScripts/ClasesAPi/LogroObtenerRequest.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[System.Serializable]
+public class LogroObtenerRequest
+{
+    public int id_usuario;
+    public int id_logro;
+}
